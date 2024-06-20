2b3043919bc1e882182ebbe7059fc720f9336a19
yuvi0915 <mechanicalyuvi1999@gmail.com>
2024-06-20 09:48:35 +0530

    Added js

---
+++ product.js
@@ -0,0 +1,38 @@
+//Selecting Side Navbar, MenuIcon
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0
+})
+
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
+
+
+//product search functionality
+
+var productcontainer = document.getElementById("productcontainer")
+var search = document.getElementById("search")
+var productlist = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredValue = event.target.value.toUpperCase()
+
+    for (count =0; count<productlist.length; count=count+1)
+        {
+            var productname = productlist[count].querySelector("h1").textContent
+
+            if (productname.toUpperCase().indexOf(enteredValue)<0) {
+                productlist[count].style.display="none"
+            }
+            else{
+                productlist[count].style.display="block"
+            }
+        }
+
+    })
+
